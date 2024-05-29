5b2543692f47a3036546c162ea964620aec2b276
Yadav <p.sudhir.kumar@accenture.com>
2024-05-29 16:22:49 +0530

    Adding Files

---
+++ src/main/java/com/adas/ParkAssist/ParkAssistApplication.java
@@ -0,0 +1,13 @@
+package com.adas.ParkAssist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkAssistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adas/ParkAssist/ParkAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adas.ParkAssist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
