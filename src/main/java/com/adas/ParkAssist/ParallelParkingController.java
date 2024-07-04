ab05bac787d421ac0e2a94a63e69693b53402635
moulikasunkishala09 <moulika.sunkishala@accenture.com>
2024-07-04 12:59:46 +0530

    #20649 improve parkassist UI

--- src/main/java/com/adas/ParkAssist/ParallelParkingController.java
+++ src/main/java/com/adas/ParkAssist/ParallelParkingController.java
@@ -1,4 +1,4 @@
-package com.adas.ParkAssist;
+ package com.adas.ParkAssist;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
