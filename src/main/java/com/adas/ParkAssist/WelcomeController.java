227fe1098f53f771179aa206efa008f7401417b3
poorvashayadav <p.sudhir.kumar@accenture.com>
2024-07-15 18:17:28 +0530

    #19690 Change Request for enhancing parallel parking

--- src/main/java/com/adas/ParkAssist/WelcomeController.java
+++ src/main/java/com/adas/ParkAssist/WelcomeController.java
@@ -8,6 +8,6 @@ public class WelcomeController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello, Welcome to Park Assist!";
+        return "<html><body><h1 style=\"font-size: 24px; font-weight: bold;\">Hello, Welcome to Park Assist!</h1><br>We are working on enhacing park assist feature.</body></html>";
     }
 }
